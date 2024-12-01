4790821ceeb725c49e63d3a50ed8bbfa0bde7949
HanniaFranco <harteaga100539@ppsc.edu.mx>
2024-12-01 10:33:07 -0600

    Footer

---
+++ src/components/CartPage/CartPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CartPage.css';
+
+function CartPage() {
+  return (
+    <div className="cart-page">
+      <h1>TU CARRITO ESTÁ VACÍO</h1>
+      <p>Su carrito está vacío.</p>
+      {/* Aquí puedes agregar más contenido relacionado con el carrito */}
+    </div>
+  );
+}
+
+export default CartPage;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import './Footer.css';
+import './Footer.css'; 
 import facebookIcon from '../../assets/images/facebook.png';
 import instagramIcon from '../../assets/images/instagram.png';
 import twitterIcon from '../../assets/images/xicon.png';
 import pinterestIcon from '../../assets/images/pinterest.png';
 
 function Footer() {
-  const [isAboutUsOpen, setIsAboutUsOpen] = useState(false);
-  const [isHelpOpen, setIsHelpOpen] = useState(false);
-  const [isServiceOpen, setIsServiceOpen] = useState(false);
+  const [isAboutUsOpen, setIsAboutUsOpen] = useState(true); 
+  const [isHelpOpen, setIsHelpOpen] = useState(true); 
+  const [isServiceOpen, setIsServiceOpen] = useState(true); //Para hacerlo abierto por defecto en pantallas grandes.
 
   const toggleSection = (section) => {
     if (section === 'about') {
@@ -24,41 +24,41 @@ function Footer() {
     <footer className="footer">
       <div className="footer-links">
         <div className="footer-section">
-          <h4 onClick={() => toggleSection('about')}>Sobre Nosotros</h4>
-          {isAboutUsOpen && (
-            <ul>
-              <li>Quienes Somos</li>
-              <li>Responsabilidad Social</li>
-              <li>Nuestras Tiendas</li>
-              <li>Trabaja con Nosotros</li>
-              <li>Sala de Prensa</li>
-            </ul>
-          )}
+          <h4 onClick={() => toggleSection('about')} className="toggle-header">
+            Sobre Nosotros {isAboutUsOpen ? '-' : '+'}
+          </h4>
+          <ul className={`footer-list ${isAboutUsOpen ? 'open' : ''}`}>
+            <li>Quienes Somos</li>
+            <li>Responsabilidad Social</li>
+            <li>Nuestras Tiendas</li>
+            <li>Trabaja con Nosotros</li>
+            <li>Sala de Prensa</li>
+          </ul>
         </div>
 
         <div className="footer-section">
-          <h4 onClick={() => toggleSection('help')}>Ayuda & Apoyo</h4>
-          {isHelpOpen && (
-            <ul>
-              <li>Información de Envío</li>
-              <li>Devolución</li>
-              <li>Reembolso</li>
-              <li>Pedidos</li>
-              <li>Rastro de Pedido</li>
-              <li>Guía de Tallas</li>
-            </ul>
-          )}
+          <h4 onClick={() => toggleSection('help')} className="toggle-header">
+            Ayuda & Apoyo {isHelpOpen ? '-' : '+'}
+          </h4>
+          <ul className={`footer-list ${isHelpOpen ? 'open' : ''}`}>
+            <li>Información de Envío</li>
+            <li>Devolución</li>
+            <li>Reembolso</li>
+            <li>Pedidos</li>
+            <li>Rastro de Pedido</li>
+            <li>Guía de Tallas</li>
+          </ul>
         </div>
 
         <div className="footer-section">
-          <h4 onClick={() => toggleSection('service')}>Servicio al Cliente</h4>
-          {isServiceOpen && (
-            <ul>
-              <li>Contáctanos</li>
-              <li>Forma de Pago</li>
-              <li>Puntos</li>
-            </ul>
-          )}
+          <h4 onClick={() => toggleSection('service')} className="toggle-header">
+            Servicio al Cliente {isServiceOpen ? '-' : '+'}
+          </h4>
+          <ul className={`footer-list ${isServiceOpen ? 'open' : ''}`}>
+            <li>Contáctanos</li>
+            <li>Forma de Pago</li>
+            <li>Puntos</li>
+          </ul>
         </div>
 
         <div className="footer-socials">
@@ -78,12 +78,12 @@ function Footer() {
       </div>
 
       <div className="footer-bottom">
-        <p>© 2024 Bloom & Glow. All Rights Reserved.</p>
         <div className="footer-bottom-links">
           <a href="/terms">Términos y Condiciones</a>
           <a href="/privacy">Política de Privacidad</a>
           <a href="/copyright">Aviso de Copyright</a>
         </div>
+        <p>© 2024 Bloom & Glow. All Rights Reserved.</p>
       </div>
     </footer>
   );
